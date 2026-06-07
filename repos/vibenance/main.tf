55c46f8f9d82987edf55b8e66a846fa3cde212d8
sean-b765 <sean.b0153@gmail.com>
2026-06-07 15:56:04 +0700

    update: cdn cache strategy

--- repos/vibenance/main.tf
+++ repos/vibenance/main.tf
@@ -47,36 +47,48 @@ resource "aws_cloudfront_origin_access_control" "this" {
 }
 
 resource "aws_cloudfront_distribution" "this" {
+  aliases = ["pfm.seanboaden.dev"]
+
   origin {
     domain_name              = aws_s3_bucket.this.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.this.id
     origin_id                = local.s3_origin_id
   }
 
-  aliases = ["pfm.seanboaden.dev"]
-
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
 
   default_cache_behavior {
+    target_origin_id = local.s3_origin_id
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
 
-    forwarded_values {
-      query_string = true
-
-      cookies {
-        forward = "none"
-      }
-    }
+    # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html#managed-cache-caching-optimized
+    cache_policy_id = data.aws_cloudfront_cache_policy.caching_optimized.id
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 0
+    max_ttl                = 300
   }
+
+  ordered_cache_behavior {
+    path_pattern     = "/assets/*"
+    target_origin_id = local.s3_origin_id
+
+    allowed_methods = ["GET", "OPTIONS"]
+    cached_methods  = ["GET", "OPTIONS"]
+
+    cache_policy_id = data.aws_cloudfront_cache_policy.caching_optimized.id
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 31536000
+    default_ttl            = 31536000
+    max_ttl                = 31536000
+    compress               = true
+  }
+
   price_class = "PriceClass_200"
 
   restrictions {
